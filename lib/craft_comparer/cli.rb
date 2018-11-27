357e1bcd197f2781d75f792482fbca3db16d25d1
Sujimichi <sujimichi@gmail.com>
2018-11-27 01:00:31 +0000

    getting command line interface working

--- lib/craft_comparer/cli.rb
+++ lib/craft_comparer/cli.rb
@@ -1,25 +1,41 @@
 require 'thor'
 require 'craft_comparer'
-require 'directory_actions'
+require 'craft_comparer/directory_actions'
 
 module CraftComparer
   class CLI < Thor
-    default_task :info
+    default_task :main
 
-    desc "info", "basic overview"
-    def info
-      puts "I am a fish"
+    desc "CRAFT", "compare given craft name/path/index with all other craft in the current directory"    
+    method_option "with", :type => :array
+    def main craft = nil
+      return invoke :help if craft.nil?
+      @dir = DirectoryActions.new 
+
+      this = @dir.select_craft craft
+      unless options[:with].nil? || options[:with].empty?
+        those = options[:with].map{|craft| @dir.select_craft craft}
+      else
+        those = @dir.files.map{|craft| @dir.select_craft craft}
+      end
+
+      those.each do |that|
+        this.compare_with that
+      end
+
     end
 
-    desc "list", "list the craft files in the current directory"
+    map %w[--list] => :list
+    desc "--list", "list all craft files in current directory"
     def list
-      DirectoryActions.list_craft
+      DirectoryActions.new.show
     end
 
-    desc "compare", "test"
-    def compare *args
-      raise [args, options].inspect
-    end
+    map %w[--version -v] => :version
+    desc "--version, -v", "Print the version"
+    def version
+      puts "CarftComparer #{CraftComparer::VERSION}"
+    end    
 
   end
 end
--- lib/craft_comparer/directory_actions.rb
+++ lib/craft_comparer/directory_actions.rb
@@ -1,16 +1,33 @@
+require 'craft_comparer'
 
 class DirectoryActions
+  attr_accessor :files
 
-  def self.list_craft
-    files = Dir.glob("*.craft")
-    if files.empty?
+  def initialize
+    @files = Dir.glob("*.craft")
+  end
+
+  def show
+    if @files.empty?
       puts "no .craft files found in current directory"
     else
-      spacer = files.count.to_s.length
-      files.each_with_index do |filename, index|
+      spacer = @files.count.to_s.length
+      @files.each_with_index do |filename, index|
         puts "#{index}#{Array.new(spacer-index.to_s.length){" "}.join} - #{filename}"
       end
     end
+  end
+
+  def select_craft craft_name_or_index
+    if @files.include?(craft_name_or_index) || File.exists?(craft_name_or_index)
+      craft_file = File.open(craft_name_or_index, "r"){|f| f.readlines}
+    elsif craft_name_or_index.to_i.to_s.eql?(craft_name_or_index) #test if string is an integer
+      if @files[craft_name_or_index.to_i] != nil
+        craft_file = File.open(@files[craft_name_or_index.to_i], "r"){|f| f.readlines}        
+      end
+    end
+    CraftComparer::Craft.new(craft_file, :output => true) if craft_file
+    nil
   end
 
 end
